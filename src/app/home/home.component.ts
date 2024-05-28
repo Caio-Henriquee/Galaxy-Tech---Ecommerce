f242c657e6b639b49d68e112e6a533a00d0649b4
Caio Henrique <caio.henriqueza7@gmail.com>
2024-05-28 15:07:37 -0300

    navbar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderContentComponent } from '../header/header-content/header-content.component';
+import { NavbarContentComponent } from '../navbar/navbar-content/navbar-content.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderContentComponent],
+  imports: [HeaderContentComponent, NavbarContentComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
