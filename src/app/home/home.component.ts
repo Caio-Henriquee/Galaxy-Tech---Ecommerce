40a4f12c6ecdf18307711c7ee27cf2d087966ca8
Caio Henrique <caio.henriqueza7@gmail.com>
2024-05-29 15:52:33 -0300

    cards_home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderContentComponent } from '../header/header-content/header-content.component';
 import { NavbarContentComponent } from '../navbar/navbar-content/navbar-content.component';
+import { CardContentComponent } from '../card/card-content/card-content.component';
+
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderContentComponent, NavbarContentComponent],
+  imports: [HeaderContentComponent, NavbarContentComponent, CardContentComponent  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
